0f242c9bde6b0c511ffaf5e8ae47dc7b001b187d
Ndiaga Lo <ndiagalo259@gmail.com>
2026-08-28 16:26:04 +0000

    feat:fix footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -111,7 +111,7 @@ export const Footer: React.FC = () => {
                   aria-label="LinkedIn FIDELE SARL"
                 >
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z" />
+                    <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 1-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z" />
                   </svg>
                 </a>
               </div>
@@ -126,7 +126,7 @@ export const Footer: React.FC = () => {
               <p className="footer-tagline">
                 Entreprise Sénégalaise de Référence en Bâtiment, Génie Civil & Transport de Matériel.
               </p>
-              <div className="reach-out-card">
+              <Link href="/contact" className="reach-out-card">
                 <div className="reach-out-icon">
                   <ArrowUpRight size={20} />
                 </div>
@@ -134,7 +134,7 @@ export const Footer: React.FC = () => {
                   <strong>Bureau d&apos;Études & Direction</strong>
                   <span>Liaison continue 7j/7 Dakar & Régions</span>
                 </div>
-              </div>
+              </Link>
             </div>
 
             <div className="footer-nav-groups">
